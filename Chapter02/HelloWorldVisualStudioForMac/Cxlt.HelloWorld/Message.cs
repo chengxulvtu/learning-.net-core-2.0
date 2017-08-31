c23fe49a5d8be093854df5e11a64f8c8af22784e
chengxulvtu <chengxulvtu@126.com>
2017-08-31 13:25:36 +0800

    HelloWorldVisualStudioForMac

---
+++ Chapter02/HelloWorldVisualStudioForMac/Cxlt.HelloWorld/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cxlt.HelloWorld
+{
+    public class Message
+    {
+        public void HelloWorld()
+        {
+            Console.WriteLine("Hello World Visual Studio for Mac.");
+        }
+    }
+}
