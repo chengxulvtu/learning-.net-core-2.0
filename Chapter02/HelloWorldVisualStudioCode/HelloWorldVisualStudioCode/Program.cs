33979b14372701dea7d6f551d75a2ee0a04fe8d5
chengxulvtu <chengxulvtu@126.com>
2017-08-31 15:31:31 +0800

    HelloWorldVisualStudioCode

---
+++ Chapter02/HelloWorldVisualStudioCode/HelloWorldVisualStudioCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Cxlt.HelloWorld;
+
+namespace HelloWorldVisualStudioCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Message().HelloWorld();
+        }
+    }
+}
