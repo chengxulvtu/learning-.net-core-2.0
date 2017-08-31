9036a269a079a66460c41c60833d688b3786154f
bobtian <nianming031223@126.com>
2017-08-31 18:26:47 +0800

    HelloWorldVisualStudio2017

---
+++ Chapter02/HelloWorldVisualStudio2017/HelloWorldVisualStudio2017/Program.cs
@@ -0,0 +1,13 @@
+﻿using Cxlt.HelloWorld;
+using System;
+
+namespace HelloWorldVisualStudio2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Message().HelloWorld();
+        }
+    }
+}
